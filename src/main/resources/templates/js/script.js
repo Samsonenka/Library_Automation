7c919edfebd7f245a83b34c2e6d5c1b6f9b84aa5
MrUnderSan <MrUnderSan@gmail.com>
2021-02-08 20:26:08 +0300

    add all pages

---
+++ src/main/resources/templates/js/script.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+
+    // Modal
+
+    $('[data-modal=add]').on('click', function() {
+        $('.overlay, #add').fadeIn('slow');
+    });
+    $('[data-modal=search]').on('click', function() {
+        $('.overlay, #search').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+       $('.overlay, #add, #search').fadeOut('slow');
+    });
+    $('.modal__button').on('click', function(){
+        $('.overlay, #add, #search').fadeOut('slow');
+     });
+    $('[data-modal=readers]').on('click', function() {
+        $('#readers').fadeIn('1');
+        $('#books, #user').fadeOut('1');
+    });
+    $('[data-modal=books]').on('click', function() {
+        $('#books').fadeIn('1');
+        $('#readers, #user').fadeOut('1');
+    });
+    $('[data-modal=user]').on('click', function() {
+        $('#user').fadeIn('1');
+        $('#readers, #books').fadeOut('1');
+    });
+
+
+});
