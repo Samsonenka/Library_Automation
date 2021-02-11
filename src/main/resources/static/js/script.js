be9a590fd2b64d88b2d72c5a3abaaf4deb25bed3
MrUnderSan <MrUnderSan@gmail.com>
2021-02-11 22:06:48 +0300

    edit buttons

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -6,14 +6,20 @@ $(document).ready(function(){
     $('[data-modal=add]').on('click', function() {
         $('.overlay, #add').fadeIn('slow');
     });
+    $('[data-modal=booksadd]').on('click', function() {
+        $('.overlay, #booksadd').fadeIn('slow');
+    });
+    $('[data-modal=readersadd]').on('click', function() {
+        $('.overlay, #readersadd').fadeIn('slow');
+    });
     $('[data-modal=search]').on('click', function() {
         $('.overlay, #search').fadeIn('slow');
     });
     $('.modal__close').on('click', function(){
-       $('.overlay, #add, #search').fadeOut('slow');
+       $('.overlay, #add, #booksadd, #readersadd, #search').fadeOut('slow');
     });
     $('.modal__button').on('click', function(){
-        $('.overlay, #add, #search').fadeOut('slow');
+        $('.overlay, #add, #booksadd, #readersadd, #search').fadeOut('slow');
      });
     $('[data-modal=readers]').on('click', function() {
         $('#readers').fadeIn('1');
