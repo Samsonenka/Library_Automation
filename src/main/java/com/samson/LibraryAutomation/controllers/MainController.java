202409e1f79254e097ac34fe5c96360cac076b61
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2021-02-11 19:12:10 +0100

    add readerController and addReader method

--- src/main/java/com/samson/LibraryAutomation/controllers/MainController.java
+++ src/main/java/com/samson/LibraryAutomation/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.samson.LibraryAutomation.controllers;
 
 import com.samson.LibraryAutomation.repo.BookRepo;
+import com.samson.LibraryAutomation.repo.ReaderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,10 +13,14 @@ public class MainController {
     @Autowired
     private BookRepo bookRepo;
 
+    @Autowired
+    private ReaderRepo readerRepo;
+
     @GetMapping("/")
     public String index(ModelMap modelMap){
 
         modelMap.put("booksList", bookRepo.findAll());
+        modelMap.put("readersList", readerRepo.findAll());
 
         return  "index";
     }
